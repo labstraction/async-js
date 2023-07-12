3f0d518616a5cac8ae1370116628fe0de2aaa4c5
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-12 12:09:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function superFunzione(){
+    console.log('sto eseguendo il codice dentro il timeout');
+}
+
+
+const pippo = 1;
+
+const pluto = 2;
+
+setTimeout(superFunzione, 20000);
+
+const paperino = pippo + pluto;
+
+const topolino = 'javascript'
+
+console.log(topolino + paperino);
+
